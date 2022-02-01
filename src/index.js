d3f2965579f59335dea761846088e32b5895eb88
Melanie Arellano <melaniesigrid@protonmail.com>
2022-02-01 11:25:01 -0600

    Correct linter errors

--- src/index.js
+++ src/index.js
@@ -1,15 +1,2 @@
-import _ from 'lodash';
 import './style.css';
-import html from "./index.html";
 
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  return element;
-}
-
-document.body.appendChild(component());
