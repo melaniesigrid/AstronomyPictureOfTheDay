625ead3264fd47caa3ed3bcc9cae5f0012a58a17
Melanie Arellano <melaniesigrid@protonmail.com>
2022-02-03 09:52:40 -0600

    Correct linter errors

--- src/count.js
+++ src/count.js
@@ -2,6 +2,6 @@ const countCards = () => {
   const myArray = document.querySelectorAll('.card');
   const count = myArray.length;
   return count;
-}
+};
 
-export { countCards as default, };
+export { countCards as default };
