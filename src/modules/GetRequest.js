ee1f8d77f662301746b680cc7b3d01cee5c5f372
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-02-06 06:47:13 +0500

    Update GetRequest.js

--- src/modules/GetRequest.js
+++ src/modules/GetRequest.js
@@ -1,6 +1,6 @@
 const baseUrl = 'https://api.nasa.gov/planetary/apod?api_key=';
 const key = 'j9gWflgyCduUULTGmKMfahoaNAbzAULMkGGuu9jN';
-const startDate = '2022-01-31';
+const startDate = '2022-01-28';
 const endDate = '2022-02-05';
 const url = `${baseUrl}${key}&start_date=${startDate}&end_date=${endDate}`;
 
